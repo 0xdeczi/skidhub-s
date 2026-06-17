2310343622587e6852a2aee8c686da36b448cec8
0xdeczi <fenvironment@protonmail.com>
2026-06-17 18:12:47 +0300

    log1

---
+++ ZombieHubReverce/step1.lua
@@ -0,0 +1,8 @@
+local Env = getfenv();
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/twiceAA/ZombieHub/refs/heads/main/Zombie-hub-Optimizate-script-Boost-Fps2"))(); 
+end);
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/twiceAA/ZombieHub/refs/heads/main/Zombie-hub-Optimizate-script-Boost-Fps"))(); 
+end);
+return;
